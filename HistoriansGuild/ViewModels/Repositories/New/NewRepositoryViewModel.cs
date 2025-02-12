f7f4ae95a88544564d483347c27b3cdcd3aafae8
Elliot Eserin <elliot@unusualtechnologies.com>
2025-02-12 23:38:22 +0000

    Add navigation to AddRepositoryView when clicking Add

--- HistoriansGuild/ViewModels/Repositories/New/NewRepositoryViewModel.cs
+++ HistoriansGuild/ViewModels/Repositories/New/NewRepositoryViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +12,13 @@ namespace HistoriansGuild.ViewModels.Repositories.New
 {
     public partial class NewRepositoryViewModel : ViewModelBase
     {
+        [ObservableProperty]
+        private ViewModelBase? currentViewModel;
+
+        [RelayCommand]
+        void AddRepository()
+        {
+            CurrentViewModel = new AddRepositoryViewModel();
+        }
     }
 }
