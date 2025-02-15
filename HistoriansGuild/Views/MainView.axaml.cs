6877602b0f25c9d972521609061067f2c021831e
Elliot Eserin <55984683+ElliotEserin@users.noreply.github.com>
2025-02-15 23:41:23 +0000

    Window customisation and custom title bar

--- HistoriansGuild/Views/MainView.axaml.cs
+++ HistoriansGuild/Views/MainView.axaml.cs
@@ -1,4 +1,7 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.VisualTree;
 
 namespace HistoriansGuild.Views;
 
@@ -7,5 +10,35 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
+
+    private void OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (VisualRoot is Window window)
+        {
+            window.BeginMoveDrag(e);
+        }
+    }
+
+    private void MinimiseButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (this.GetVisualRoot() is Window window)
+        {
+            window.WindowState = WindowState.Minimized;
+        }
+    }
+
+    private void MaximiseButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (this.GetVisualRoot() is Window window)
+        {
+            window.WindowState = window.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        var window = this.GetVisualRoot() as Window;
+        window?.Close();
     }
 }
--- HistoriansGuild/Views/MainWindow.axaml.cs
+++ HistoriansGuild/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 ﻿using Avalonia.Controls;
+using System.Runtime.InteropServices;
+using System;
 
 namespace HistoriansGuild.Views;
 
@@ -7,5 +9,33 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        ExtendClientAreaToDecorationsHint = true;
+        ExtendClientAreaChromeHints = Avalonia.Platform.ExtendClientAreaChromeHints.NoChrome;
+        ExtendClientAreaTitleBarHeightHint = -1;
+
+        //Opened += (_, _) => RemoveRoundedCorners();
+        //Resized += (_, _) => RemoveRoundedCorners();
     }
+
+    private void RemoveRoundedCorners()
+    {
+        int DWMWA_WINDOW_CORNER_PREFERENCE = 33;
+        int DWMWCP_DONOTROUND = 1;
+
+        if (OperatingSystem.IsWindows())
+        {
+            var handle = TryGetPlatformHandle();
+            if (handle == null) return;
+
+            var hwnd = handle.Handle;
+            if (hwnd != IntPtr.Zero)
+            {
+                DwmSetWindowAttribute(hwnd, DWMWA_WINDOW_CORNER_PREFERENCE, ref DWMWCP_DONOTROUND, sizeof(int));
+            }
+        }
+    }
+
+    [DllImport("dwmapi.dll")]
+    private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
 }
